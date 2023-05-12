93e93c6982105d16ed5c000787685aab94a8773c
yvnnik <83207610+yvnnik@users.noreply.github.com>
2023-05-12 11:35:27 +0200

    updated code to open an url

--- com.yannikquellmalz.vizrtgraphics.sdPlugin/app.js
+++ com.yannikquellmalz.vizrtgraphics.sdPlugin/app.js
@@ -1,8 +1,43 @@
+var websocket = null;
+var pluginUUID = null;
+
+var vizAction = {
+
+  type: "com.yannikquellmalz.vizrtgraphics.action",
+
+  onKeyDown: function (context, settings, coordinates, userDesiredState) {
+    console.log("Key pressed by user!");
+
+    this.OpenURL();
+  },
+
+  SetSettings: function (context, settings) {
+    var json = {
+      "event": "setSettings",
+      "context": context,
+      "payload": settings
+    };
+
+    websocket.send(JSON.stringify(json));
+  },
+
+  OpenURL: function () {
+    var json = {
+      "event": "openUrl",
+      "payload": {
+        "url": "https://www.ba-sachsen.de",
+      }
+    };
+
+    websocket.send(JSON.stringify(json));
+  }
+};
+
 function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo) {
   pluginUUID = inPluginUUID
 
   // Open the web socket
-  websocket = new WebSocket("ws://localhost:" + inPort);
+  websocket = new WebSocket("ws://127.0.0.1:" + inPort);
 
   function registerPlugin(inPluginUUID) {
     var json = {
@@ -20,51 +55,32 @@ function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, in
 
   websocket.onmessage = function (evt) {
     // Received message from Stream Deck
-    /*
     var jsonObj = JSON.parse(evt.data);
     var event = jsonObj['event'];
     var action = jsonObj['action'];
     var context = jsonObj['context'];
-    var jsonPayload = jsonObj['payload'] || {};
 
     if (event == "keyDown") {
+      var jsonPayload = jsonObj['payload'];
       var settings = jsonPayload['settings'];
       var coordinates = jsonPayload['coordinates'];
       var userDesiredState = jsonPayload['userDesiredState'];
-      myAction.onKeyDown(context, settings, coordinates, userDesiredState);
+      counterAction.onKeyDown(context, settings, coordinates, userDesiredState);
     }
     else if (event == "keyUp") {
+      var jsonPayload = jsonObj['payload'];
       var settings = jsonPayload['settings'];
       var coordinates = jsonPayload['coordinates'];
       var userDesiredState = jsonPayload['userDesiredState'];
-      myAction.onKeyUp(context, settings, coordinates, userDesiredState);
+      counterAction.onKeyUp(context, settings, coordinates, userDesiredState);
     }
     else if (event == "willAppear") {
+      var jsonPayload = jsonObj['payload'];
       var settings = jsonPayload['settings'];
       var coordinates = jsonPayload['coordinates'];
-      myAction.onWillAppear(context, settings, coordinates);
+      counterAction.onWillAppear(context, settings, coordinates);
     }
-    else if (event == "sendToPlugin") {
-
-      if (jsonPayload.hasOwnProperty('background-image')) {
-
-        const imageName = jsonPayload['background-image'];
-
-        loadImageAsDataUri(`${imageName}.png`, function (imgUrl) {
-          var json = {
-            "event": "setImage",
-            "context": context,
-            "payload": {
-              image: imgUrl || "",
-              target: DestinationEnum.HARDWARE_AND_SOFTWARE
-            }
-          };
-          websocket.send(JSON.stringify(json));
-        })
-
-      }
-    }*/
-  }; 
+  };
 
   websocket.onclose = function () {
     // Websocket is closed
